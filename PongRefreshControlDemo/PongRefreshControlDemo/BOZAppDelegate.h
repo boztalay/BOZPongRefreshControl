6c22347da5d8d04e4692ac66bd67ce97878e0d68
Ben Oztalay <boztalay@gmail.com>
2013-12-06 14:50:07 -0500

    Started the demo project

---
+++ PongRefreshControlDemo/PongRefreshControlDemo/BOZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BOZAppDelegate.h
+//  PongRefreshControlDemo
+//
+//  Created by Ben Oztalay on 12/6/13.
+//  Copyright (c) 2013 Ben Oztalay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BOZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PongRefreshControlDemo/PongRefreshControlDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PongRefreshControlDemo
+//
+//  Created by Ben Oztalay on 12/6/13.
+//  Copyright (c) 2013 Ben Oztalay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BOZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BOZAppDelegate class]));
+    }
+}
